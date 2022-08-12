0047bcaec6f9b0508b68ece7128858a8e973d5c0
KirkNovos <kurulka@mail.ru>
2022-08-12 16:21:20 +0300

    Example Sockets Server-Client

---
+++ Socket Server/Socket Server/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Socket_Server
+{
+    class Program
+    {
+        static int port = 8001;// порт для приема входящих запросов
+        static void Main(string[] args)
+        {
+            // получение адреса для запуска сокета
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+
+            //создание сокета
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                // связь сокета с локальной точкой, по которой будут принимать данные
+                listenSocket.Bind(ipPoint);
+
+                // начало прослушивания
+                listenSocket.Listen(10);
+
+                Console.WriteLine("Сервер запущен. Ожидание подключения");
+
+                while (true)
+                {
+                    Socket arrived = listenSocket.Accept();
+                    //получение сообщения
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;//кол-во полученных байтов
+                    byte[] data = new byte[256];//буфер получаемых данных
+
+                    do
+                    {
+                        bytes = arrived.Receive(data);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (arrived.Available > 0);
+
+                    Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+
+                    //отправка ответа
+                    string msg = "сообщение доставлено";
+                    data = Encoding.Unicode.GetBytes(msg);
+                    arrived.Send(data);
+                    arrived.Shutdown(SocketShutdown.Both);
+                    arrived.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ SocketClient/SocketClient/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketClient
+{
+    class Program
+    {
+
+        //адрес и порт сервера к которым будем подлкючаться
+        static int port = 8001;
+        static string adress = "127.0.01";
+
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(adress), port);
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                //подключение к удаленному хосту
+                socket.Connect(ipPoint);
+                Console.WriteLine("введите сообщение:");
+                string msg = Console.ReadLine();
+                byte[] data = Encoding.Unicode.GetBytes(msg);
+                socket.Send(data);
+
+                //получение ответа
+                data = new byte[256];//буфер для ответа
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0;
+
+                do
+                {
+                    bytes = socket.Receive(data, data.Length, 0);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (socket.Available > 0);
+
+                //закрытие сокета
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
